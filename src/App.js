9a78f86a3434489497c70b26232a86d7949b97cf
forrestmorrison <forrestmorrison@gmail.com>
2023-07-28 17:29:32 -0500

    created react app, installed dependencies

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          Bitcoin Budget
+      </header>
+    </div>
+  );
+}
+
+export default App;
