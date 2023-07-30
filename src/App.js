89b160e7711216fbb07133405ee056943ebbbea0
forrestmorrison <forrestmorrison@gmail.com>
2023-07-29 22:40:53 -0500

    time amount input code

--- src/App.js
+++ src/App.js
@@ -11,31 +11,10 @@ function App() {
   const [currencyAmt, setCurrencyAmt] = useState("")
   const [currency, setCurrency] = useState("")
   const [frequency, setFrequency] = useState("")
+  const [timeAmt, setTimeAmt] = useState("")
+  const [timePeriod, setTimePeriod] = useState("")
   
   const currentPrice = coin[0]
-
-  const onCurrencyAmtChange = (e) => {
-    setCurrencyAmt(e.target.value)
-  }
-
-  const onCurrencyChange = (e) => {
-    setCurrency(e.target.value)
-  }
-
-  const onFrequencyChange = (e) => {
-    setFrequency(e.target.value)
-  }
-
-  const url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=bitcoin&order=market_cap_desc&per_page=1&page=1&sparkline=false&locale=en&precision=2"
-
-  useEffect(() => {
-    axios.get(url).then((response) => {
-      setCoin(response.data)
-      console.log(response.data[0])
-    }).catch((error) => {
-      console.log(error)
-    })
-  }, [])
 
   const currencies = [
     {
@@ -66,6 +45,56 @@ function App() {
         label: "once per month"
     }
   ]
+
+  const timePeriods = [
+    {
+        value: "days",
+        label: "days"
+    },
+    {
+        value: "weeks",
+        label: "weeks"
+    },
+    {
+        value: "months",
+        label: "months"
+    },
+    {
+        value: "years",
+        label: "years"
+    }
+  ]
+
+  const onCurrencyAmtChange = (e) => {
+    setCurrencyAmt(e.target.value)
+  }
+
+  const onCurrencyChange = (e) => {
+    setCurrency(e.target.value)
+  }
+
+  const onFrequencyChange = (e) => {
+    setFrequency(e.target.value)
+  }
+
+  const onTimeAmtChange = (e) => {
+    setTimeAmt(e.target.value)
+  }
+
+  const onTimePeriodChange = (e) => {
+      setTimePeriod(e.target.value)
+  }
+
+  const url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=bitcoin&order=market_cap_desc&per_page=1&page=1&sparkline=false&locale=en&precision=2"
+
+  useEffect(() => {
+    axios.get(url).then((response) => {
+      setCoin(response.data)
+      console.log(response.data[0])
+    }).catch((error) => {
+      console.log(error)
+    })
+  }, [])
 
   return (
     <div className="App">
@@ -135,6 +164,41 @@ function App() {
                       {option.label}
                   </MenuItem>
               ))}
+          </TextField>
+        </div>
+        <h5>total amount of time to keep making recurring purchase:</h5>
+        <div className="input-line">
+          <TextField 
+            id="outlined-basic" 
+            label="time amount" 
+            variant="outlined"
+            onChange={onTimeAmtChange}
+            value={timeAmt}
+            required
+            sx={{
+                m: 1,
+                width: "300px"
+            }}
+          />
+          <TextField 
+            id="outlined-select"
+            select
+            label="time period"
+            defaultValue=""
+            helperText="select time period"
+            variant="outlined"
+            onChange={onTimePeriodChange}
+            value={timePeriod}
+            required
+            sx={{
+                m: 1
+            }}
+          >
+            {timePeriods.map((option) => (
+              <MenuItem key={option.value} value={option.value}>
+                {option.label}
+              </MenuItem>
+            ))}
           </TextField>
         </div>
       </div>
